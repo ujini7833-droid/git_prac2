075f450469e98ea1690c759dc301b89bcecdbd13
Bae, Yujin <ujini7833@gmail.com>
2025-09-29 20:43:18 +0900

    conflict 해결

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -2,8 +2,8 @@ package git_test2;
 
 public class Main {
 	public static void main(String[] args) {
-		Hello h = new Hello("hello world");
-		h.say();
+		Hello Hello= new Hello("hello world");
+		Hello.say();
 	}
 
 }
